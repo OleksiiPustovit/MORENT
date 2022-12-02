89eb2b21f36eaffbe63500d4e389bc9e6352144c
Oleksii Pustovit <91pustovit91@gmail.com>
2022-12-02 21:54:47 +0200

    implement filter hide

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # MORENT
 
-[DEMO LINK](https://oleksiipustovit.github.io/MORENT/)
+Car Rent Landing
+
+- [DEMO LINK](https://oleksiipustovit.github.io/MORENT/)
+
+- [Figma](https://www.figma.com/file/uA7kRTj5367RW5JCChUc9z/Car-Rent-Website-Design---Pickolab-Studio-(Community)-(Copy)?node-id=9%3A15394&t=I7Hxfshhi554YnB3-0)
+
+Implemented mobile version (375px) and desctop version (1440px)
+________________________________________________________
+
+Used technologies
+
+- HTML
+- SCSS
+- JS
+- Bootstrap
+
+________________________________________________________
+
+
+Also you can install the project in few steps
+
+- clone repository
+
+- npm install
+
+- npm start 
--- src/script/main.js
+++ src/script/main.js
@@ -1,10 +1,8 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
-import 'swiper/swiper-bundle.css';
+import Swiper from 'swiper';
+// import 'swiper/swiper-bundle.css';
 
 // eslint-disable-next-line no-unused-vars
 const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Pagination],
-  slidesPerView: 'auto',
   spaceBetween: 24,
   loop: true,
   clickable: true,
@@ -16,9 +14,29 @@ const swiper = new Swiper('.swiper', {
       slidesPerView: 1,
     },
   },
+});
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+document.getElementById('filter-btn').onclick
+= function() {
+    document.getElementById('sidebar').classList.toggle('sidebar-active');
+  };
+
+// eslint-disable-next-line no-unused-vars
+const swiper1 = new Swiper('.mySwiper', {
+  spaceBetween: 10,
+  slidesPerView: 3,
+  freeMode: true,
+});
+
+// eslint-disable-next-line no-unused-vars
+const swiper2 = new Swiper('.mySwiper2', {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: swiper,
   },
 });
